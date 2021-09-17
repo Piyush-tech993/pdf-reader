694af64ee4d82c7f1d9ee4b0b663c90057a5d503
Piyush Soni <sonipiyush993@gmail.com>
2021-09-17 23:12:22 +0530

    Added all

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download PDF using php</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body{
+            height: 100vh;
+            display: grid;
+            place-items: center;
+        }
+    </style>
+</head>
+<body>
+        <main>
+            <h2 style="color: #d7385e;">Welcome To PDF Reader</h2>
+            <p><b>Click below to download pdf</b></p>
+            <a href="download.php?id=androidcheatsheet" target="_blank">Download PDF Now</a>
+        </main>
+        
+</body>
+</html>
